91b6eae2e6cdd4ed7aacdfbbbf709902e200e8f3
Khaled Rahman Ayon <khaledrahman.ayon@publicissapient.com>
2020-02-18 17:19:31 +0100

    added scratch are progress percentage

--- ScratchCardDemo/MainVC.swift
+++ ScratchCardDemo/MainVC.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, ScratchCardDelegate {
+    
+    
+    
+    let viewBehindScratchCard = UIView()
+    let previewView = ScratchCardView(image: UIImage(named: "test")!)
+    let progressLabel = UILabel()
 
     override func viewDidLoad() {
-        view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
         
-       let viewBehindScratchCard = UIView()
+        previewView.scratchDelegate = self
+        
+        let resetButton = UIButton(type: .system)
+        resetButton.setTitle("Reset Image", for: .normal)
+        resetButton.backgroundColor = .white
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+        resetButton.addTarget(self, action: #selector(handleReserButton), for: .touchUpInside)
+        view.addSubview(resetButton)
+        
+
         viewBehindScratchCard.isUserInteractionEnabled = true
         viewBehindScratchCard.backgroundColor = .red
       
@@ -35,8 +50,8 @@ class MainVC: UIViewController {
         couponView.leadingAnchor.constraint(equalTo: viewBehindScratchCard.leadingAnchor).isActive = true
         couponView.trailingAnchor.constraint(equalTo: viewBehindScratchCard.trailingAnchor).isActive = true
         
-        let previewView = ScratchCardView()
-        previewView.backgroundColor = .clear
+        
+//        previewView.backgroundColor = .clear
         previewView.translatesAutoresizingMaskIntoConstraints = false
         previewView.isUserInteractionEnabled = true
         viewBehindScratchCard.addSubview(previewView)
@@ -45,7 +60,32 @@ class MainVC: UIViewController {
         previewView.bottomAnchor.constraint(equalTo: viewBehindScratchCard.bottomAnchor).isActive = true
         previewView.leadingAnchor.constraint(equalTo: viewBehindScratchCard.leadingAnchor).isActive = true
         previewView.trailingAnchor.constraint(equalTo: viewBehindScratchCard.trailingAnchor).isActive = true
-
+        
+        resetButton.topAnchor.constraint(equalTo: viewBehindScratchCard.bottomAnchor, constant: 40).isActive = true
+        resetButton.heightAnchor.constraint(equalToConstant: 46).isActive = true
+        resetButton.widthAnchor.constraint(equalTo: viewBehindScratchCard.widthAnchor, constant: -40).isActive = true
+        resetButton.centerXAnchor.constraint(equalTo: viewBehindScratchCard.centerXAnchor).isActive = true
+        
+        progressLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(progressLabel)
+        progressLabel.textAlignment = .center
+        progressLabel.textColor = .white
+        progressLabel.font = UIFont.systemFont(ofSize: 30)
+        
+        progressLabel.topAnchor.constraint(equalTo: resetButton.bottomAnchor, constant: 20).isActive = true
+        progressLabel.leadingAnchor.constraint(equalTo: viewBehindScratchCard.leadingAnchor, constant: 20).isActive = true
+        progressLabel.trailingAnchor.constraint(equalTo: viewBehindScratchCard.trailingAnchor, constant: -20).isActive = true
+        progressLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+    }
+    
+    @objc func handleReserButton() {
+        previewView.reset()
+        progressLabel.text = String.init(format: "-- %d %@ --", 0 , "%")
+    }
+    
+    func scratch(percentage value: Int) {
+        progressLabel.text = String.init(format: "-- %d %@ --", value , "%")
     }
     
 }
--- ScratchCardDemo/ScratchCardView.swift
+++ ScratchCardDemo/ScratchCardView.swift
@@ -8,6 +8,9 @@
 
 
 import UIKit
+protocol ScratchCardDelegate {
+    func scratch(percentage value:Int)
+}
 
 class ScratchCardView: UIView {
     
@@ -16,49 +19,91 @@ class ScratchCardView: UIView {
     var context:CGContext!
     var co_ordinates = [(startPoint:CGPoint,endPoint:CGPoint)]()
     var swiped = false
-    
-    
-    func reset() {
-        
-        co_ordinates.removeAll()
-        min_x = 1000
-        max_x = 0
-        min_y = 1000
-        max_y = 0
-        self.setNeedsDisplay()
-    }
-    
+    var scratchDelegate:ScratchCardDelegate?
+
     var min_x:Int = 1000
     var max_x:Int = 0
     
     var min_y:Int = 1000
     var max_y:Int = 0
-//    var scratchImage: UIImage?
     
-    var overlayImage:UIImage!
-
+    let scratchCardImage:UIImage
+    
+    init(image: UIImage) {
+        self.scratchCardImage = image
+        super.init(frame: .zero)
+         backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-                
-//        guard let image = scratchImage
-//            else {
-//                fatalError("PLEASE ADD ADD SCRATCH IMAGE")
-//        }
-        
-        overlayImage = UIImage(named: "test")
-        overlayImage.draw(in: self.frame)
-        
+        calculateUserScratchArea()
+        scratchCardImage.draw(in: self.frame)
         context = UIGraphicsGetCurrentContext()
         
-        for each in co_ordinates {
-            
-            self.drawLineFrom(fromPoint:each.startPoint , toPoint: each.endPoint)
-            
+        for point in co_ordinates {
+            self.drawLineFrom(fromPoint:point.startPoint , toPoint: point.endPoint)
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.startPoint = touches.first?.location(in: self)
+        storeStartCoordiante()
+        
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.endPoint = touches.first?.location(in: self)
+        
+        
+        
+        guard self.frame.contains(self.endPoint) else { return }
+        
+        storeEndCoordinate()
+        
+        swiped = true
+        co_ordinates.append((self.startPoint,self.endPoint))
+        
+        self.startPoint = endPoint
+        
+        setNeedsDisplay()
+        
+    }
     
-    
+    fileprivate func calculateUserScratchArea() {
+
+        // Area of rectange = Length * Width
+        
+        let scratchCardArea =  self.bounds.width*self.bounds.height
+        
+        let maxLengthOfScratchSurface = (max_y - min_y)
+        let maxWidthOfScratchSurface = (max_x - min_x)
+        
+        let scratchedArea = maxWidthOfScratchSurface * maxLengthOfScratchSurface
+        
+        guard scratchedArea > 0 else {
+            return
+        }
+        
+        let scratchPercentage = ( CGFloat(scratchedArea) / CGFloat(scratchCardArea)  ) * 100
+        print(scratchPercentage)
+        
+        //FIXME: MAKE ME STABLE
+        if scratchPercentage < 95 {
+            
+            scratchDelegate?.scratch(percentage: Int(scratchPercentage))
+            
+        } else if scratchPercentage < 200  {
+            
+            scratchDelegate?.scratch(percentage: 100)
+        }
+        
+    }
     fileprivate func storeStartCoordiante() {
         let temp_x = Int(self.startPoint.x)
         let temp_y = Int(self.startPoint.y)
@@ -72,60 +117,24 @@ class ScratchCardView: UIView {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.startPoint = touches.first?.location(in: self)
-        
-        storeStartCoordiante()
-        
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        if !swiped {
-            co_ordinates.append((self.startPoint,self.startPoint))
-            setNeedsDisplay()
-        }
-        
-    }
-    
     fileprivate func storeEndCoordinate() {
-        let temp_x = Int(self.endPoint.x)
-        let temp_y = Int(self.endPoint.y)
-        
-        if temp_x>max_x {
-            max_x = temp_x
-        }
-        if temp_y>max_y {
-            max_y = temp_y
-        }
-        
-        if temp_x<min_x  {
-            min_x = temp_x
-        }
-        if temp_y<min_y {
-            min_y = temp_y
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        self.endPoint = touches.first?.location(in: self)
-        
-        guard  self.frame.contains(self.endPoint)
-            else {
-                return
-        }
-        
-        storeEndCoordinate()
-        
-        swiped = true
-        co_ordinates.append((self.startPoint,self.endPoint))
-        
-        self.startPoint = endPoint
-        
-        setNeedsDisplay()
-        
-    }
+           let temp_x = Int(self.endPoint.x)
+           let temp_y = Int(self.endPoint.y)
+           
+           if temp_x>max_x {
+               max_x = temp_x
+           }
+           if temp_y>max_y {
+               max_y = temp_y
+           }
+           
+           if temp_x<min_x  {
+               min_x = temp_x
+           }
+           if temp_y<min_y {
+               min_y = temp_y
+           }
+       }
     
     fileprivate func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
         
@@ -134,6 +143,18 @@ class ScratchCardView: UIView {
         context.setBlendMode(.clear)
         context.setLineCap(.round)
         context.addLine(to: toPoint)
-        context.strokePath()        
+        context.strokePath()
+        
+    }
+}
+
+extension ScratchCardView {
+    func reset() {
+        co_ordinates.removeAll()
+        min_x = 1000
+        max_x = 0
+        min_y = 1000
+        max_y = 0
+        self.setNeedsDisplay()
     }
 }
